7bc2c91b9d4b629af2ae0aaceef6f6e97943fd1a
DC* <dc@syncr.com.ar>
2014-11-29 04:08:58 -0300

    Removed error/access logs redirection

--- conf/nginx/heroku.conf.php
+++ conf/nginx/heroku.conf.php
@@ -35,10 +35,8 @@ http {
         
         root "<?=getenv('DOCUMENT_ROOT')?:getenv('HEROKU_APP_DIR')?:getcwd()?>";
         
-        #error_log stderr;
-        #access_log /tmp/heroku.nginx_access.<?=getenv('PORT')?:'8080'?>.log;
-        error_log /dev/stdout notice;
-        access_log /dev/stderr;
+        error_log stderr;
+        access_log /tmp/heroku.nginx_access.<?=getenv('PORT')?:'8080'?>.log;
         
         include "<?=getenv('HEROKU_PHP_NGINX_CONFIG_INCLUDE')?>";
         
